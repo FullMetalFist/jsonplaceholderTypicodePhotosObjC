fb0c8db101da450bd6eeb10f95b103f6fe025566
Michael Vilabrera <m.vilabrera@gmail.com>
2018-03-26 10:30:00 -0400

    add PhotoModel files

---
+++ PhotosLargeAndSmall/Model/PhotoModel.m
@@ -0,0 +1,13 @@
+//
+//  PhotoModel.m
+//  PhotosLargeAndSmall
+//
+//  Created by Michael Vilabrera on 3/26/18.
+//  Copyright © 2018 Michael Vilabrera. All rights reserved.
+//
+
+#import "PhotoModel.h"
+
+@implementation PhotoModel
+
+@end
