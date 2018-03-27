59336f8d47f6c367595c3ad4e0c2e21fc55529dd
Michael Vilabrera <m.vilabrera@gmail.com>
2018-03-27 10:46:44 -0400

    add DetailVC

---
+++ PhotosLargeAndSmall/Controller/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  PhotosLargeAndSmall
+//
+//  Created by Michael Vilabrera on 3/27/18.
+//  Copyright © 2018 Michael Vilabrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
